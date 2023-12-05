0a82631b9956b5ab2678492234e7815a7a33e8d0
kun zhu <zk1987313@gmail.com>
2023-12-05 11:17:16 -0600

    update migrations

---
+++ giggles_wiggles/database/migrations/2023_12_05_171006_create_line_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('line_items', function (Blueprint $table) {
+            $table->id();
+            $table->order_id();
+            $table->product_id();
+            $table->unit_price();
+            $table->name();
+            $table->quantity();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('line_items');
+    }
+};
