11ba87290a4ff018a7593fab2272bb9930d8cd2e
kun zhu <zk1987313@gmail.com>
2023-12-06 10:27:38 -0600

    finished face images and new images storage folder

--- giggles_wiggles/database/seeders/DatabaseSeeder.php
+++ giggles_wiggles/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,26 +32,30 @@ class DatabaseSeeder extends Seeder
         // Create 36 products
          \App\Models\Product::factory(36)->create();
 
-        // Generate 3 images for each product
+        // Generate 5 images for each product
         for($i=1; $i<=36; $i++){
-            \App\Models\Image::factory()->create([
-                'product_id' => $i,
-                'image' => fake()->image(
-                    width: 600, height: 600, dir: storage_path('app/public/images/'), 
-                    fullPath: false),
-            ]);
-            \App\Models\Image::factory()->create([
-                'product_id' => $i,
-                'image' => fake()->image(
-                    width: 600, height: 600, dir: storage_path('app/public/images/'), 
-                    fullPath: false),
-            ]);
-            \App\Models\Image::factory()->create([
-                'product_id' => $i,
-                'image' => fake()->image(
-                    width: 600, height: 600, dir: storage_path('app/public/images/'), 
-                    fullPath: false),
-            ]);
+            $image_name1 = 'product_' . $i . '_image_01.png';
+            $image_name2 = 'product_' . $i . '_image_02.png';
+            $image_name3 = 'product_' . $i . '_image_03.png';
+            $image_name4 = 'product_' . $i . '_image_05.png';
+            $image_name5 = 'product_' . $i . '_image_05.png';
+            $image_file1 = fake()->image(width: 600, height: 600, fullPath: false, dir: storage_path('app/public/images/'));
+            $image_file2 = fake()->image(width: 600, height: 600, fullPath: false, dir: storage_path('app/public/images/'));
+            $image_file3 = fake()->image(width: 600, height: 600, fullPath: false, dir: storage_path('app/public/images/'));
+            $image_file4 = fake()->image(width: 600, height: 600, fullPath: false, dir: storage_path('app/public/images/'));
+            $image_file5 = fake()->image(width: 600, height: 600, fullPath: false, dir: storage_path('app/public/images/'));
+            if(PHP_OS === 'Linux'){
+                Storage::disk('images')->put($image_name1, File::get($image_file1));
+                Storage::disk('images')->put($image_name2, File::get($image_file2));
+                Storage::disk('images')->put($image_name3, File::get($image_file3));
+                Storage::disk('images')->put($image_name4, File::get($image_file4));
+                Storage::disk('images')->put($image_name5, File::get($image_file5));
+            }
+            \App\Models\Image::factory()->create([ 'product_id' => $i, 'image' => $image_name1 ]);
+            \App\Models\Image::factory()->create([ 'product_id' => $i, 'image' => $image_name2 ]);
+            \App\Models\Image::factory()->create([ 'product_id' => $i, 'image' => $image_name3 ]);
+            \App\Models\Image::factory()->create([ 'product_id' => $i, 'image' => $image_name4 ]);
+            \App\Models\Image::factory()->create([ 'product_id' => $i, 'image' => $image_name5 ]);
         }
     }
 }
