5fbb8b1350a9f0c6e64659b0dce57a847c7cc497
Cherish Abella <cherishptolentino@gmail.com>
2023-12-06 14:21:33 -0600

    Fixed the css

--- giggles_wiggles/resources/views/apparel/index.blade.php
+++ giggles_wiggles/resources/views/apparel/index.blade.php
@@ -3,12 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
-    <link rel="stylesheet" href="css/style.css">
-   <!-- Scripts -->
-   @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
   <title>Giggles Wiggles</title>
+
+
+  @vite(['resources/css/style.css'])
 </head>
 <body>
 
@@ -310,6 +310,8 @@
   </div>
 </footer>
 
-@vite(['resources/js/app.js'])
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
