4674f371897c6ab9354fa6095c286d9e408031b3
Loresa Bueckert <lovedandforgiven98@gmail.com>
2023-12-14 20:01:54 -0600

    successfully got card type javascript to work

--- giggles_wiggles/resources/js/app.js
+++ giggles_wiggles/resources/js/app.js
@@ -1,11 +1,6 @@
 import './bootstrap';
 
-    document.getElementById("shipping_address_different").addEventListener('click', checkShipping());
-    document.getElementById("visa").addEventListener('click', cardType('visa'));
-    document.getElementById("mastercard").addEventListener('click', cardType('mastercard'));
-    document.getElementById("amex").addEventListener('click', cardType('amex'));
-
-    function checkShipping(){
+    document.getElementById("shipping_address_different").addEventListener('click', function(){
         if(document.getElementById("shipping_address_different").checked){
             document.getElementById("address").readOnly = true;
             document.getElementById("address").style.opacity = .2;
@@ -32,11 +27,28 @@ import './bootstrap';
             document.getElementById("postal_code").readOnly = false;
             document.getElementById("postal_code").style.opacity = 1;
         }
-        }
+    });
 
-    function cardType($type){
-        alert("hi");
-        document.getElementById("card_type").value = $type;
+    document.getElementById("visa").addEventListener('click', function(){
+        document.getElementById("card_type").value = 'visa';
         document.getElementById("visa").style.borderBottom = "3px solid green";
-        }
+        document.getElementById("mastercard").style.borderBottom = 'none';
+        document.getElementById("amex").style.borderBottom = "none";
+        
+    });
+
+    document.getElementById("mastercard").addEventListener('click', function(){
+        document.getElementById("card_type").value = 'mastercard';
+        document.getElementById("mastercard").style.borderBottom = "3px solid green";
+        document.getElementById("visa").style.borderBottom = "none";
+        document.getElementById("amex").style.borderBottom = "none";
+    });
+
+    document.getElementById("amex").addEventListener('click', function(){
+        document.getElementById("card_type").value = 'amex';
+        document.getElementById("amex").style.borderBottom = "3px solid green";
+        document.getElementById("mastercard").style.borderBottom = "none";
+        document.getElementById("visa").style.borderBottom = "none";
+    });
+
 
