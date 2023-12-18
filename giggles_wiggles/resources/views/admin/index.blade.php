e6419eb247f4e406a0ea16f9ddb997ce6fd74c13
Yesha Karmakar <karmakar-y@webmail.uwinnipeg.ca>
2023-12-17 22:42:59 -0600

    Merging the dashboard with the updated registry and checkout

--- giggles_wiggles/resources/views/admin/index.blade.php
+++ giggles_wiggles/resources/views/admin/index.blade.php
@@ -9,21 +9,21 @@
             <div id="statistics">
                 <div class="stat_card stat_card_bg_one text-center">
                     <h4 class="text-uppercase font-weight-bold">Users</h4>
-                    <p>{{count($customers)}}</p>
+                    <p class="big-font">{{count($customers)}}</p>
                 </div>
                 <div class="stat_card stat_card_bg_two text-center">
                     <h4 class="text-uppercase">Categories</h4>
-                    <p>{{ count($categories) }}</p>
+                    <p class="big-font">{{ count($categories) }}</p>
                 </div>
 
                 <div class="stat_card stat_card_bg_three text-center">
                     <h4 class="text-uppercase">Orders</h4>
-                    <p>{{ count($orders) }}</p>
+                    <p class="big-font">{{ count($orders) }}</p>
                 </div>
 
                 <div class="stat_card stat_card_bg_four text-center">
                     <h4 class="text-uppercase">Products</h4>
-                    <p>{{ count($products) }}</p>
+                    <p class="big-font">{{ count($products) }}</p>
                 </div>                
 
             </div>
