49c79a9eed83ea5c5bef719747e5c86c603d7776
Loresa Bueckert <lovedandforgiven98@gmail.com>
2023-12-17 21:52:41 -0600

    added regex for postal code

--- giggles_wiggles/app/Http/Controllers/Auth/RegisterController.php
+++ giggles_wiggles/app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,7 @@ class RegisterController extends Controller
             'address' => ['required', 'string','max:255'],
             'city' => ['required', 'string', 'max:255'],
             'province' => ['required'],
-            'postal_code' => ['required', 'string', 'max:255'],
+            'postal_code' => ['required', 'regex:/^[A-z][0-9][A-z]\s?[0-9][A-z][0-9]$/gm'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
